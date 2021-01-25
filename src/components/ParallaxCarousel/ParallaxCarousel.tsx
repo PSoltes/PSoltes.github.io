e7ac16a42abbc04c32449d2ae4ea7ae6e96e7231
Pavol Soltes <soltes.pavol@gmail.com>
2021-01-25 21:51:04 +0100

    add swipe to carousel on mobile

--- src/components/ParallaxCarousel/ParallaxCarousel.tsx
+++ src/components/ParallaxCarousel/ParallaxCarousel.tsx
@@ -4,6 +4,7 @@ import { Parallax } from "components";
 import React, { ReactNode, useState } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import * as S from "./styled";
+import Hammer from "react-hammerjs";
 
 interface ParallaxCarouselProps {
   id?: string;
@@ -19,6 +20,15 @@ const ParallaxCarousel = ({
   overlayColor,
 }: ParallaxCarouselProps) => {
   const [selectedImageIndex, setSelectedImageIndex] = useState<number>(0);
+  const goToNext = () => {
+    const nextIndex = (selectedImageIndex + 1) % content.length;
+    setSelectedImageIndex(nextIndex);
+  };
+  const goToPrevious = () => {
+    const nextIndex =
+      selectedImageIndex - 1 > -1 ? selectedImageIndex - 1 : content.length - 1;
+    setSelectedImageIndex(nextIndex);
+  };
 
   // useEffect(() => {
   //   setTimeout(() => {
@@ -28,88 +38,88 @@ const ParallaxCarousel = ({
   // }, [selectedImageIndex, content]);
 
   return (
-    <div id={id} style={{ height: "100vh" }}>
-      <TransitionGroup>
-        <CSSTransition
-          classNames="slide"
-          timeout={{ enter: 1100, exit: 1100 }}
-          key={selectedImageIndex}
-        >
-          <S.ParallaxWrapperForCarousel>
-            <Parallax
-              key={selectedImageIndex}
-              backgroundImageUrl={content[selectedImageIndex].path}
-              overlayColor={overlayColor}
-            >
-              <Grid container style={{ height: "100%" }}>
-                <Hidden mdDown>
-                  <Grid
-                    item
-                    container
-                    md={3}
-                    alignContent="center"
-                    justify="center"
-                  >
-                    {
-                      <IconButton
-                        color="primary"
-                        onClick={() => {
-                          const nextIndex =
-                            selectedImageIndex - 1 > -1
-                              ? selectedImageIndex - 1
-                              : content.length - 1;
-                          setSelectedImageIndex(nextIndex);
-                        }}
-                        aria-label="right-carousel"
-                      >
-                        <ChevronLeft fontSize="large" />
-                      </IconButton>
-                    }
+    <Hammer
+      onSwipe={({direction, deltaX}:HammerInput) => {
+        if (direction === 4 && deltaX > 50) {
+          goToNext();
+        } else if (direction === 2 && deltaX < -50) {
+          goToPrevious()
+        }
+      }}
+    >
+      <div id={id} style={{ height: "100vh" }}>
+        <TransitionGroup>
+          <CSSTransition
+            classNames="slide"
+            timeout={{ enter: 1100, exit: 1100 }}
+            key={selectedImageIndex}
+          >
+            <S.ParallaxWrapperForCarousel>
+              <Parallax
+                key={selectedImageIndex}
+                backgroundImageUrl={content[selectedImageIndex].path}
+                overlayColor={overlayColor}
+              >
+                <Grid container style={{ height: "100%" }}>
+                  <Hidden mdDown>
+                    <Grid
+                      item
+                      container
+                      md={3}
+                      alignContent="center"
+                      justify="center"
+                    >
+                      {
+                        <IconButton
+                          color="primary"
+                          onClick={goToPrevious}
+                          aria-label="right-carousel"
+                        >
+                          <ChevronLeft fontSize="large" />
+                        </IconButton>
+                      }
+                    </Grid>
+                  </Hidden>
+                  <Grid item direction="column" xs={12} md={6}>
+                    {content[selectedImageIndex].imageContent}
                   </Grid>
-                </Hidden>
-                <Grid item direction="column" xs={12} md={6}>
-                  {content[selectedImageIndex].imageContent}
-                </Grid>
 
-                <Hidden mdDown>
-                  <Grid
-                    item
-                    container
-                    md={3}
-                    alignContent="center"
-                    justify="center"
-                  >
-                    {
-                      <IconButton
-                        color="primary"
-                        onClick={() => {
-                          const nextIndex =
-                            (selectedImageIndex + 1) % content.length;
-                          setSelectedImageIndex(nextIndex);
-                        }}
-                        aria-label="right-carousel"
-                      >
-                        <ChevronRight fontSize="large" />
-                      </IconButton>
-                    }
-                  </Grid>
-                </Hidden>
-              </Grid>
-            </Parallax>
-            <div className="parallax-carousel_dot-container">
-              {content.map((item, index) => (
-                <div
-                  className={`parallax-carousel_dot ${
-                    index === selectedImageIndex &&
-                    "parallax-carousel_dot_selected"
-                  }`}
-                ></div>
-              ))}
-            </div>
-          </S.ParallaxWrapperForCarousel>
-        </CSSTransition>
-      </TransitionGroup>
-    </div>
+                  <Hidden mdDown>
+                    <Grid
+                      item
+                      container
+                      md={3}
+                      alignContent="center"
+                      justify="center"
+                    >
+                      {
+                        <IconButton
+                          color="primary"
+                          onClick={goToNext}
+                          aria-label="right-carousel"
+                        >
+                          <ChevronRight fontSize="large" />
+                        </IconButton>
+                      }
+                    </Grid>
+                  </Hidden>
+                </Grid>
+              </Parallax>
+              <div className="parallax-carousel_dot-container">
+                {content.map((_, index) => (
+                  <div
+                    className={`parallax-carousel_dot ${
+                      index === selectedImageIndex &&
+                      "parallax-carousel_dot_selected"
+                    }`}
+                  ></div>
+                ))}
+              </div>
+            </S.ParallaxWrapperForCarousel>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+    </Hammer>
   );
 };
 
